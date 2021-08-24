3dd5745ee0a5d4730424f233b4e6de9c89079e1e
Lucas Silva <lucascavalcante@hotmail.com>
2021-08-24 19:52:50 -0300

    Project create

---
+++ src/test/java/com/factsolution/persona_api/PersonaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factsolution.persona_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
